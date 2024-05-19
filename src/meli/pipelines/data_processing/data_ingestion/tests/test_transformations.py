e2411d07d0a8cab6e999466e7e8b87f8c715250e
felipe tellez <felipe_tellez13@outlook.com>
2024-05-18 20:33:34 -0500

    commit

--- src/meli/pipelines/data_processing/data_ingestion/tests/test_transformations.py
+++ src/meli/pipelines/data_processing/data_ingestion/tests/test_transformations.py
@@ -1,15 +1,3 @@
-# Copyright (c) 2016 - present
-# QuantumBlack Visual Analytics Ltd (a McKinsey company).
-# All rights reserved.
-#
-# This software framework contains the confidential and proprietary information
-# of QuantumBlack, its affiliates, and its licensors. Your use of these
-# materials is governed by the terms of the Agreement between your organization
-# and QuantumBlack, and any unauthorized use is forbidden. Except as otherwise
-# stated in the Agreement, this software framework is for your internal use
-# only and may only be shared outside your organization with the prior written
-# permission of QuantumBlack.
-# pylint: disable=invalid-name
 """data_integration tests."""
 
 import logging
